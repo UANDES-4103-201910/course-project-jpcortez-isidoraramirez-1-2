5bdd9ba4e40e7d2c41503a8eabefa9d58baf049b
Isidora <iaramirez@miuandes.cl>
2019-04-12 09:45:50 -0400

    validations redi

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   belongs_to :administrator
   has_one :profile
   has_many :posts
+  validates :password, :length => { :in => 8..12 }, :format => { :with => /\A[\sa-z0-9]+\Z/i }
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
