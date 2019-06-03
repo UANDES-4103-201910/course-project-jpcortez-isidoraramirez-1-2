03d66012c61d3288a66ca917aeff46c937fa43fe
isidoraramirez <iaramirez@miuandes.cl>
2019-06-02 20:06:53 -0400

    a

--- project/app/controllers/comments_controller.rb
+++ project/app/controllers/comments_controller.rb
@@ -7,5 +7,15 @@ class CommentsController < ApplicationController
     @comment.save
     redirect_to '/'
   end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = comment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:post_id)
+    end
 
 end
